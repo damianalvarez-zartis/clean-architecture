a683febe591e493b0cf9ad4c12c942bbe0e10615
Damian Alvarez Smiet <damian.alvarez@evotix.com>
2022-12-23 10:37:41 +0100

    Remove Ardalis template things

--- src/Clean.Architecture.Core/Interfaces/IDeleteContributorService.cs
+++
@@ -1,8 +0,0 @@
-using Ardalis.Result;
-
-namespace Clean.Architecture.Core.Interfaces;
-
-public interface IDeleteContributorService
-{
-    public Task<Result> DeleteContributor(int contributorId);
-}
