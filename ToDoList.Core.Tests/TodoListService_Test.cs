4a5e535f2840e28cfc3361f51172d1e0b45775d9
Damian Alvarez Smiet <damian.alvarez@evotix.com>
2023-01-15 19:44:22 +0100

    Improved, still WIP

--- ToDoList.Core.Tests/TodoListService_Test.cs
+++ ToDoList.Core.Tests/TodoListService_Test.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
-using ToDoList.Core.Abstractions.Services;
 using ToDoList.Core.Models.Domain;
 using ToDoList.Core.Models.Requests;
 using ToDoList.Core.Models.Results;
@@ -8,6 +7,8 @@ using Moq;
 using Xunit;
 using ToDoList.Core.Abstractions.Repositories;
 using ToDoList.Core.Abstractions.Validators;
+using ToDoList.Core.Services;
+using System;
 
 namespace ToDoList.Core.Tests
 {
@@ -16,27 +17,57 @@ namespace ToDoList.Core.Tests
         [Fact]
         public async Task CreateListAsync_ShouldReturnExpectedResult()
         {
+            // Arrange
             var createValidatorMock = new Mock<ICreateListRequestValidator>();
             var addValidatorMock = new Mock<IAddTaskToListRequestValidator>();
             var repositoryMock = new Mock<ITodoListRepository>();
-
-            var service = new Mock<ITodoListService>();
-            service.Setup(x => x.CreateListAsync(It.IsAny<CreateListRequest>(), It.IsAny<CancellationToken>()))
-                .Returns(new ValueTask<Result<TodoList>>());
-
-            // Arrange
-            var request = new CreateListRequest { Title = "Test List" };
             var cancellationToken = CancellationToken.None;
 
+            var service = new TodoListService(createValidatorMock.Object, addValidatorMock.Object, repositoryMock.Object);
+
+            var createListRequest = new CreateListRequest
+            {
+                Title = "Test List"
+            };
+
+            var list = await service.CreateListAsync(createListRequest, cancellationToken);
+
             // Act
-            var result = await service.Object.CreateListAsync(request, cancellationToken);
+            var result = await service.CreateListAsync(createListRequest, cancellationToken);
 
             // Assert
             Assert.IsType<Result<TodoList>>(result);
-            service.Verify(x => x.CreateListAsync(It.IsAny<CreateListRequest>(), It.IsAny<CancellationToken>()), Times.Once);
-            //Assert.Equal("Test List", result.Value?.Title);
+            Assert.IsType<TodoList>(result.Value);
+            Assert.NotNull(result.Value);
+            createValidatorMock.Verify(x => x.Validate(createListRequest), Times.AtLeastOnce);
+            addValidatorMock.Verify(x => x.Validate(null), Times.Never);
+            repositoryMock.Verify(x => x.CreateListAsync(list.Value, cancellationToken), Times.Once);
         }
 
+        [Fact]
+        public async Task CreateListAsync_ShouldThrowExceptionIfNullRequest()
+        {
+            // Arrange
+            var createValidatorMock = new Mock<ICreateListRequestValidator>();
+            var addValidatorMock = new Mock<IAddTaskToListRequestValidator>();
+            var repositoryMock = new Mock<ITodoListRepository>();
+            var cancellationToken = CancellationToken.None;
+
+            createValidatorMock.Setup(x => x.Validate(null)).Throws<Exception>();
+
+
+            var service = new TodoListService(createValidatorMock.Object, addValidatorMock.Object, repositoryMock.Object);
+
+            CreateListRequest createListRequest = null;
+
+            // Act
+            void Act() => service.CreateListAsync(createListRequest, cancellationToken);
+
+            // Assert
+            var exception = Assert.Throws<Exception>(Act);
+            Assert.Equal("Title can not be null.", exception.Message);
+
+        }
     }
 }
 
